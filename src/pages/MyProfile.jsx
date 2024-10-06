35f73b38025c72e90b0d4dff2d476e624d448992
Arif Khan <khanarif2738@gmail.com>
2024-10-06 10:25:05 +0530

    Created state object

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {assets} from '../assets/assets'
 const MyProfile = () => {
+
+    const [userData, setUserData] = useState({
+      name: "Arif khan",
+      image: assets.profile_pic,
+      email: 'khanarif2738@gmail.com',
+      phone: '91 7991518306',
+      address: {
+        line1: '27th Cross, Gorakhhpur',
+        line2: 'Circle, Paryatan Bhawan, India'
+      },
+      gender: 'Male',
+      dob: '2000-09-10'
+    })
+
   return (
     <div>
         
+      <img src={userData.image} alt=""/>
+
     </div>
   )
 }
