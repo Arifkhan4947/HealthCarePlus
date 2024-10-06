09d1e5efc40a064a778a1aef18b190389578384b
Arif Khan <khanarif2738@gmail.com>
2024-10-06 11:29:55 +0530

    Tailwind CSS

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -40,15 +40,15 @@ const MyProfile = () => {
             ? <input className='bg-gray-100 max-w-52' type='text' value={userData.phone} onChange={e => setUserData(prev => ({...prev,phone:e.target.value}))}/> 
             : <p className='text-blue-400'>{userData.phone}</p>
           }
-          <p>Address:</p>
+          <p className='font-medium'>Address:</p>
           {
             isEdit
             ? <p>
-              <input onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line1: e.target.value}}))} value={userData.address.line1} type="text"/>
+              <input className='bg-gray-50' onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line1: e.target.value}}))} value={userData.address.line1} type="text"/>
               <br/>
-              <input onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line2: e.target.value}}))} value={userData.address.line2} type="text"/>
+              <input className='bg-gray-50' onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line2: e.target.value}}))} value={userData.address.line2} type="text"/>
             </p>
-            : <p>
+            : <p className='text-gray-500'>
               {userData.address.line1}
               <br/>
               {userData.address.line2}
@@ -57,8 +57,8 @@ const MyProfile = () => {
         </div>
       </div>
       <div>
-        <p>BASIC INFORMATION</p>
-        <div>
+        <p className='text-neutral-500 underline mt-3'>BASIC INFORMATION</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700'>
           <p>Gender:</p>
           {
             isEdit 
